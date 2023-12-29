2fba212b3656aab9faa9b25929f957e5d764e05f
J Vance <justinleevance94@gmail.com>
2023-12-29 13:33:44 -0500

    First Pokemon Render

---
+++ index.js
@@ -0,0 +1,32 @@
+fetch("https://api.pokemontcg.io/v2/cards?set.series:base").then((resp) => resp.json())
+    .then((dataObj) => renderNavBar(dataObj));
+
+function renderNavBar (dataObj){
+    // console.log(dataObj.data);
+    let cardArray = dataObj.data;
+    const navForPoke = document.querySelector("#navBar")
+
+
+    navForPoke.textContent = "";
+    navForPoke.style.display = "flex";
+    navForPoke.style.height = 151;
+    navForPoke.style.overflowX = "scroll"; 
+
+
+    console.log(navForPoke);
+    cardArray.forEach( cardObj => {
+        const img = document.createElement("img");
+        const imgSrc = cardObj.images.small;
+
+        img.src = imgSrc;
+        img.style.margin = 3;
+        
+
+
+        
+        navForPoke.appendChild(img);
+        
+        
+    });
+        
+    };
